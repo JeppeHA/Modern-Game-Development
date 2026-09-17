3933a65a779d889763b2f0819b2a512dd91209d3
Jeppe <jeppe.holmandersen@gmail.com>
2026-09-17 11:27:34 +0200

    Multiplayer added and switch to horror map

---
+++ Source/MGD/MGD.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MGD.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MGD, "MGD" );
